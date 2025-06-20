5d52876343b94cb1c01970d89cbb41e33308be8c
tendryAxel <128225840+tendryAxel@users.noreply.github.com>
2025-06-20 21:14:51 +0300

    refactor: size unit
    * refactor: use size unit
    
    * chore: handle empty backup folder target

--- src/pybackup/type.py
+++ src/pybackup/type.py
@@ -1,12 +1,50 @@
 import re
+from dataclasses import dataclass
 from datetime import datetime
+from enum import Enum
 from typing import List
 
 from pybackup.utils import TemporarySetLocal
 
 
+# Todo: better handling for size
+# Todo: update to python 3.11 to use the new StrEnum
+class SizeUnit(str, Enum):
+    bytes = "bytes"
+    KB = "KB"
+    MB = "MB"
+    GB = "GB"
+    TB = "TB"
+
+
+@dataclass
+class Size:
+    value: float
+    unit: SizeUnit
+
+    def __init__(self, value: float | str, unit: SizeUnit = SizeUnit.bytes):
+        self.value = value if isinstance(value, float) else float(value)
+        self.unit = unit
+
+    def __eq__(self, other):
+        if not isinstance(other, Size):
+            return False
+        return self.to_bytes() == other.to_bytes()
+
+    def to_bytes(self) -> float:
+        """Convert the size to bytes for comparison"""
+        multipliers = {
+            SizeUnit.bytes: 1,
+            SizeUnit.KB: 1024,
+            SizeUnit.MB: 1024**2,
+            SizeUnit.GB: 1024**3,
+            SizeUnit.TB: 1024**4,
+        }
+        return self.value * multipliers[self.unit]
+
+
 class Increment:
-    def __init__(self, time: datetime, size: int, cumulative_size: int):
+    def __init__(self, time: datetime, size: Size, cumulative_size: Size):
         self.time = time
         self.size = size
         self.cumulative_size = cumulative_size
@@ -24,7 +62,6 @@ class Increment:
         )
 
 
-# Todo: handle empty backup target location
 def parse_increments(text: str) -> List[Increment]:
     lines = text.strip().split("\n")
     increments = []
@@ -35,13 +72,24 @@ def parse_increments(text: str) -> List[Increment]:
 
         # Match line using regex
         match = re.match(
-            r"(\w{3} \w{3}\s+\d+ \d{2}:\d{2}:\d{2} \d{4})\s+(\d+) bytes\s+(\d+) bytes",
+            (
+                r"(\w{3} \w{3}\s+\d+ \d{2}:\d{2}:\d{2} \d{4})\s+"  # Date
+                r"(\d+)\s*(bytes|B|KB|MB|GB|TB|kilobytes|megabytes|gigabytes|terabytes)?\s+"  # Size
+                r"(\d+)\s*(bytes|B|KB|MB|GB|TB|kilobytes|megabytes|gigabytes|terabytes)?"  # Cumulative size
+            ),
             line,
+            re.IGNORECASE,
         )
         if match:
-            time_str, size_str, cum_size_str = match.groups()
+            time_str, size_str, size_unit, cum_size_str, cum_size_unit = match.groups()
+            size_unit = SizeUnit[size_unit] if size_unit else SizeUnit.bytes
+            cum_size_unit = SizeUnit[cum_size_unit] if cum_size_unit else SizeUnit.bytes
             with TemporarySetLocal():
                 dt = datetime.strptime(time_str, "%a %b %d %H:%M:%S %Y")
-            increments.append(Increment(dt, int(size_str), int(cum_size_str)))
+            increments.append(
+                Increment(
+                    dt, Size(size_str, size_unit), Size(cum_size_str, cum_size_unit)
+                )
+            )
 
     return increments
--- src/tests/src/type_test.py
+++ src/tests/src/type_test.py
@@ -2,7 +2,7 @@ import datetime
 import locale
 
 
-from pybackup.type import parse_increments, Increment
+from pybackup.type import parse_increments, Increment, Size, SizeUnit
 from tests.utils import get_random_locale, repeat_test
 
 
@@ -10,22 +10,35 @@ def test_parse_increments():
     input_text = """
           Time                 Size         Cumulative size 
 ------------------------ ----------------- -----------------
-Sat Jun  7 07:27:44 2025          91 bytes         177 bytes
+Sat Jun  7 07:27:44 2025          92 bytes         178 bytes
+Sat Jun  7 07:27:45 2025          91 KB            177 KB
 Sat Jun  7 07:37:38 2025          86 bytes          86 bytes  (current mirror)
 """
 
     increments = parse_increments(input_text)
     assert (
         Increment(
-            time=datetime.datetime(2025, 6, 7, 7, 27, 44), size=91, cumulative_size=177
+            time=datetime.datetime(2025, 6, 7, 7, 27, 44),
+            size=Size(92),
+            cumulative_size=Size(178),
         )
         == increments[0]
     )
     assert (
         Increment(
-            time=datetime.datetime(2025, 6, 7, 7, 37, 38), size=86, cumulative_size=86
+            time=datetime.datetime(2025, 6, 7, 7, 27, 45),
+            size=Size(91, SizeUnit.KB),
+            cumulative_size=Size(177, SizeUnit.KB),
         )
         == increments[1]
+    )
+    assert (
+        Increment(
+            time=datetime.datetime(2025, 6, 7, 7, 37, 38),
+            size=Size(86),
+            cumulative_size=Size(86),
+        )
+        == increments[2]
     )
 
 
@@ -40,7 +53,9 @@ Sat Jun  7 07:37:38 2025          86 bytes          86 bytes  (current mirror)
     increments = parse_increments(input_text)
     assert (
         Increment(
-            time=datetime.datetime(2025, 6, 7, 7, 37, 38), size=86, cumulative_size=86
+            time=datetime.datetime(2025, 6, 7, 7, 37, 38),
+            size=Size(86),
+            cumulative_size=Size(86),
         )
         == increments[0]
     )
